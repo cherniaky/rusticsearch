39603cb90cd3b201c6ce5c6f2f9ddbeaf40559f6
CherniakYura <juracherniak@gmail.com>
2023-08-23 00:55:52 +0300

    Start reading the docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "docs.gl/gl4/glClear.xhtml";
+
+    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: could not read file {file_path}: {err}");
+        exit(1);
+    });
+    eprintln!("Lenght of {file_path} is {lenght}", lenght = content.len());
 }
