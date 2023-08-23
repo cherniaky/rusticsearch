489d42c113c70b00b2aae00b908c0c46af53a2a4
CherniakYura <juracherniak@gmail.com>
2023-08-23 18:39:17 +0300

    Read dir

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-use std::fs;
-use std::process::exit;
+use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io;
+use std::path::{Path, PathBuf};
+use xml::reader::{EventReader, XmlEvent};
 
-fn main() {
-    let file_path = "docs.gl/gl4/glClear.xhtml";
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
+    let mut content = String::new();
+    for event in er.into_iter() {
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+            content.push_str(" ");
+        }
+    }
+    Ok(content)
+}
 
-    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
-        eprintln!("ERROR: could not read file {file_path}: {err}");
-        exit(1);
-    });
-    eprintln!("Lenght of {file_path} is {lenght}", lenght = content.len());
+fn main() -> io::Result<()> {
+    let dir_path = "docs.gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
+
+    for file in dir {
+        let file_path = file?.path();
+        let content = read_entire_xml_file(&file_path)?;
+        println!("{file_path:?} => {size}", size = content.len());
+    }
+
+
+    Ok(())
 }
