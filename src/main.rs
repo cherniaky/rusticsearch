d2b7522b55403d1cace8e6f3608fac23c8c60139
CherniakYura <juracherniak@gmail.com>
2023-08-31 00:37:47 +0300

    Initial implementation of multithreading

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ use std::path::Path;
 use std::process::ExitCode;
 use std::result::Result;
 use std::str;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use xml::common::{Position, TextPosition};
 use xml::reader::{EventReader, XmlEvent};
 
 mod model;
 use model::*;
-mod server;
 mod lexer;
+mod server;
 pub mod snowball;
 
 fn parse_entire_txt_file(file_path: &Path) -> Result<String, ()> {
@@ -94,7 +96,7 @@ fn save_model_as_json(model: &InMemoryModel, index_path: &str) -> Result<(), ()>
 
 fn add_folder_to_model(
     dir_path: &Path,
-    model: &mut dyn Model,
+    model: Arc<Mutex<InMemoryModel>>,
     skipped: &mut usize,
 ) -> Result<(), ()> {
     let dir = fs::read_dir(dir_path).map_err(|err| {
@@ -121,22 +123,47 @@ fn add_folder_to_model(
             );
         })?;
 
+        let last_modified = file
+            .metadata()
+            .map_err(|err| {
+                eprintln!(
+                    "ERROR: could not get the metadata of file {file_path}: {err}",
+                    file_path = file_path.display()
+                );
+            })?
+            .modified()
+            .map_err(|err| {
+                eprintln!(
+                    "ERROR: could not get the last modification date of file {file_path}: {err}",
+                    file_path = file_path.display()
+                )
+            })?;
+
         if file_type.is_dir() {
-            add_folder_to_model(&file_path, model, skipped)?;
+            add_folder_to_model(&file_path, Arc::clone(&model), skipped)?;
             continue 'next_file;
         }
 
-        println!("Indexing {:?}...", &file_path);
+        let mut model = model.lock().unwrap();
+        if model.requires_reindexing(&file_path, last_modified)? {
+            println!("Indexing {:?}...", &file_path);
 
-        let content = match parse_entire_file_by_extension(&file_path) {
-            Ok(content) => content.chars().collect::<Vec<_>>(),
-            Err(()) => {
-                *skipped += 1;
-                continue 'next_file;
-            }
-        };
+            let content = match parse_entire_file_by_extension(&file_path) {
+                Ok(content) => content.chars().collect::<Vec<_>>(),
+                Err(()) => {
+                    *skipped += 1;
+                    continue 'next_file;
+                }
+            };
 
-        model.add_document(file_path, &content)?;
+            model.add_document(file_path, last_modified, &content)?;
+        } else {
+            println!(
+                "Ignoring {file_path} cause we already indexed it",
+                file_path = file_path.display()
+            );
+            *skipped += 1;
+        }
     }
 
     Ok(())
@@ -147,7 +174,7 @@ fn usage(program: &str) {
     eprintln!("Subcommands:");
     eprintln!("    index <folder>                  index the <folder> and save the index to index.json file");
     eprintln!("    search <index-file> <query>     search <query> within the <index-file>");
-    eprintln!("    serve <index-file> [address]    start local HTTP server with Web Interface");
+    eprintln!("    serve <folder> [address]        start local HTTP server with Web Interface");
 }
 
 fn entry() -> Result<(), ()> {
@@ -173,6 +200,7 @@ fn entry() -> Result<(), ()> {
     })?;
 
     match subcommand.as_str() {
+        /*
         "index" => {
             let dir_path = args.next().ok_or_else(|| {
                 usage(&program);
@@ -242,29 +270,49 @@ fn entry() -> Result<(), ()> {
             }
             Ok(())
         }
+        */
         "serve" => {
-            let index_path = args.next().ok_or_else(|| {
+            assert!(!use_sqlite_mode);
+            let dir_path = args.next().ok_or_else(|| {
                 usage(&program);
-                eprintln!("ERROR: no path to index is provided for {subcommand} subcommand");
+                eprintln!("ERROR: no folder is provided for {subcommand} subcommand");
             })?;
+
+            let index_path = "index.json";
 
             let address = args.next().unwrap_or("127.0.0.1:6969".to_string());
 
-            if use_sqlite_mode {
-                let model = SqliteModel::open(Path::new(&index_path))?;
-
-                server::start(&address, &model)
-            } else {
+            let exists = Path::new(index_path).try_exists().map_err(|err| {
+                eprintln!("ERROR: could not check the existence of file {index_path}: {err}");
+            })?;
+            let model: Arc<Mutex<InMemoryModel>>;
+            if exists {
                 let index_file = File::open(&index_path).map_err(|err| {
                     eprintln!("ERROR: could not open index file {index_path}: {err}");
                 })?;
 
-                let model: InMemoryModel = serde_json::from_reader(index_file).map_err(|err| {
-                    eprintln!("ERROR: could not parse index file {index_path}: {err}");
-                })?;
-
-                server::start(&address, &model)
+                model = Arc::new(Mutex::new(serde_json::from_reader(index_file).map_err(
+                    |err| {
+                        eprintln!("ERROR: could not parse index file {index_path}: {err}");
+                    },
+                )?));
+            } else {
+                model = Arc::new(Mutex::new(Default::default()));
             }
+
+            {
+                let model = Arc::clone(&model);
+                thread::spawn(move || {
+                    let mut skipped = 0;
+
+                    add_folder_to_model(Path::new(&dir_path), Arc::clone(&model), &mut skipped)
+                        .unwrap();
+                    let model = model.lock().unwrap();
+
+                    save_model_as_json(&model, index_path).unwrap();
+                });
+            }
+            server::start(&address, Arc::clone(&model))
         }
         _ => {
             usage(&program);
--- src/model.rs
+++ src/model.rs
@@ -1,12 +1,19 @@
+use super::lexer::Lexer;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::result::Result;
-use super::lexer::Lexer;
+use std::time::SystemTime;
 
 pub trait Model {
     fn search_query(&self, query: &[char]) -> Result<Vec<(PathBuf, f32)>, ()>;
-    fn add_document(&mut self, path: PathBuf, content: &[char]) -> Result<(), ()>;
+    fn requires_reindexing(&mut self, path: &Path, last_modified: SystemTime) -> Result<bool, ()>;
+    fn add_document(
+        &mut self,
+        path: PathBuf,
+        last_modified: SystemTime,
+        content: &[char],
+    ) -> Result<(), ()>;
 }
 
 pub struct SqliteModel {
@@ -80,7 +87,20 @@ impl Model for SqliteModel {
         todo!()
     }
 
-    fn add_document(&mut self, path: PathBuf, content: &[char]) -> Result<(), ()> {
+    fn requires_reindexing(
+        &mut self,
+        _path: &Path,
+        _last_modified: SystemTime,
+    ) -> Result<bool, ()> {
+        Ok(true)
+    }
+
+    fn add_document(
+        &mut self,
+        path: PathBuf,
+        _last_modified: SystemTime,
+        content: &[char],
+    ) -> Result<(), ()> {
         let terms = Lexer::new(content).collect::<Vec<_>>();
 
         let doc_id = {
@@ -163,20 +183,44 @@ impl Model for SqliteModel {
 
 type DocFreq = HashMap<String, usize>;
 type TermFreq = HashMap<String, usize>;
-#[derive(Default, Deserialize, Serialize)]
-struct Doc {
+#[derive(Deserialize, Serialize)]
+pub struct Doc {
     tf: TermFreq,
     count: usize,
+    last_modified: SystemTime,
 }
 type Docs = HashMap<PathBuf, Doc>;
 
 #[derive(Default, Deserialize, Serialize)]
 pub struct InMemoryModel {
-    docs: Docs,
+    pub docs: Docs,
     df: DocFreq,
 }
 
+impl InMemoryModel {
+    fn remove_document(&mut self, file_path: &Path) {
+        if let Some(doc) = self.docs.remove(file_path) {
+            for t in doc.tf.keys() {
+                if let Some(f) = self.df.get_mut(t) {
+                    *f -= 1;
+                }
+            }
+        }
+    }
+}
+
 impl Model for InMemoryModel {
+    fn requires_reindexing(
+        &mut self,
+        file_path: &Path,
+        last_modified: SystemTime,
+    ) -> Result<bool, ()> {
+        if let Some(doc) = self.docs.get(file_path) {
+            return Ok(doc.last_modified < last_modified);
+        }
+        return Ok(true);
+    }
+
     fn search_query(&self, query: &[char]) -> Result<Vec<(PathBuf, f32)>, ()> {
         let mut result = Vec::new();
         let tokens = Lexer::new(&query).collect::<Vec<_>>();
@@ -192,28 +236,35 @@ impl Model for InMemoryModel {
         Ok(result)
     }
 
-    fn add_document(&mut self, file_path: PathBuf, content: &[char]) -> Result<(), ()> {
+    fn add_document(
+        &mut self,
+        file_path: PathBuf,
+        last_modified: SystemTime,
+        content: &[char],
+    ) -> Result<(), ()> {
+        self.remove_document(&file_path);
+
         let mut tf = TermFreq::new();
 
         let mut count = 0;
-        for term in Lexer::new(content) {
-            if let Some(freq) = tf.get_mut(&term) {
-                *freq += 1;
+        for t in Lexer::new(content) {
+            if let Some(f) = tf.get_mut(&t) {
+                *f += 1;
             } else {
-                tf.insert(term, 1);
+                tf.insert(t, 1);
             }
             count += 1;
         }
 
         for t in tf.keys() {
-            if let Some(freq) = self.df.get_mut(t) {
-                *freq += 1;
+            if let Some(f) = self.df.get_mut(t) {
+                *f += 1;
             } else {
                 self.df.insert(t.to_string(), 1);
             }
         }
 
-        self.docs.insert(file_path, Doc { count, tf });
+        self.docs.insert(file_path, Doc { count, tf, last_modified });
         Ok(())
     }
 }
@@ -229,4 +280,3 @@ fn compute_idf(t: &str, n: usize, df: &DocFreq) -> f32 {
     let m = df.get(t).cloned().unwrap_or(1) as f32;
     (n / m).log10()
 }
-
