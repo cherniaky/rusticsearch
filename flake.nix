9599b5bec90dbfcf8e3cde13877375a5502d7da2
Yurii Cherniak <juracherniak@gmail.com>
2024-02-27 23:37:54 +0100

    Add nixos stuff (pain to set up)

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let 
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+	devShells.${system}.default = pkgs.mkShell {
+		buildInputs = with pkgs; [
+			cargo rustc 
+			dbus
+			glib
+			cairo
+			poppler
+		];	
+		
+		nativeBuildInputs = with pkgs; [
+        pkg-config
+    ];
+    dbus = pkgs.dbus;
+	};
+  };
+}
